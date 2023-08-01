a9c8f334aac11ad2884e02478f7422ded9f7836c
ANDyouNo <younofl312w@gmail.com>
2023-08-01 16:44:37 +0300

    f

--- js/app.js
+++ js/app.js
@@ -4,7 +4,24 @@ const dayInput = document.querySelector('.day')
 
 const button = document.querySelector('button')
 
+const errMsg = document.querySelectorAll('.error')
+
 button.addEventListener('click', function(){
-    console.log(yearInput.value)
+    if (parseInt(yearInput.value) > 2023) {
+    errMsg[2].innerText = 'must be a valid date'
+    }
+    if (parseInt(monthInput.value) > 12) {
+        errMsg[1].innerText = 'must be a valid date'
+    }
+    if (parseInt(dayInput.value) > 31) {
+        errMsg[0].innerText = 'must be a valid date'
+    }
 })
+
+function error() {
+    errMsg.innerText = 'error'
+}
+
+
+
 
