df494b7506d0431ae452cc1ce525df09e3949495
ANDyouNo <younofl312w@gmail.com>
2023-08-01 16:21:49 +0300

    f

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const yearInput = document.querySelector('.year')
+const monthInput = document.querySelector('.month')
+const dayInput = document.querySelector('.day')
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', function(){
+    console.log(yearInput.value)
+})
+
