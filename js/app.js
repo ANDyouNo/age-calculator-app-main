1991853068b4e164b3ef23e8c89cdb3253384e46
ANDyouNo <younofl312w@gmail.com>
2023-08-02 23:07:33 +0300

    elem fix

--- js/app.js
+++ js/app.js
@@ -1,51 +1,66 @@
-const inputs = [document.querySelector('.year'), document.querySelector('.month'), document.querySelector('.day')]
-const outputs = [document.querySelector('.year-out'), document.querySelector('.month-out'), document.querySelector('.day-out')]
+const inputs = [document.querySelector('.day'), document.querySelector('.month'), document.querySelector('.year')]
+const outputs = [document.querySelector('.day-out'), document.querySelector('.month-out'), document.querySelector('.year-out')]
 const inputHeader = document.querySelectorAll('.input__header')
-
 const button = document.querySelector('button')
-let check = 0
-var today = new Date()
-
-var now = today.toLocaleDateString('en-US')
-
-const dateValues = now.split('/')
-
 const errMsg = document.querySelectorAll('.error')
 
-button.addEventListener('click', error)
+// date format DD.MM.YY
+let today = new Date()
+let rawDate = today.toLocaleDateString('ru')
+rawDate = rawDate.split('.')
 
-function error() {
-    errMsg.forEach((value, index) => {
-        if (inputs[0].value > 2023 || inputs[1].value > 12 || inputs[2].value > 31) {
-            errMsg[index].innerText = 'Must be a valid date'
-            inputs[index].classList.add('error-border')
-            inputHeader[index].classList.add('error')
-        } else if (inputs[0].value == '' || inputs[1].value == '' || inputs[2].value == '') {
+button.addEventListener('click', function() {
+    let check = 0
+    if (parseInt(inputs[0].value) > 31) {
+        errMsg[0].innerText = 'Must be a valid date'
+        inputs[0].classList.add('error-border')
+        inputHeader[0].classList.add('error')
+        check += 1
+    }
+    else {
+        errMsg[0].innerText = ''
+        inputs[0].classList.remove('error-border')
+        inputHeader[0].classList.remove('error')
+    }
+    if (inputs[1].value > 12) {
+        errMsg[1].innerText = 'Must be a valid date'
+        inputs[1].classList.add('error-border')
+        inputHeader[1].classList.add('error')
+        check += 1
+    }
+    else {
+        errMsg[1].innerText = ''
+        inputs[1].classList.remove('error-border')
+        inputHeader[1].classList.remove('error')
+    }
+    if (parseInt(inputs[2].value) > parseInt(rawDate[2])) {
+        errMsg[2].innerText = 'Must be a valid date'
+        inputs[2].classList.add('error-border')
+        inputHeader[2].classList.add('error')
+        check += 1
+    }
+    else {
+        errMsg[2].innerText = ''
+        inputs[2].classList.remove('error-border')
+        inputHeader[2].classList.remove('error')
+    }
+    inputs.forEach((value, index) => {
+        if (inputs[index].value == '') {
             errMsg[index].innerText = 'This field is required'
             inputs[index].classList.add('error-border')
             inputHeader[index].classList.add('error')
+            check += 1
         }
-        else {
+        else if (check == 0) {
             errMsg[index].innerText = ''
             inputs[index].classList.remove('error-border')
             inputHeader[index].classList.remove('error')
-            check = 1
         }
     })
-}
-
-button.addEventListener('click', output)
-
-function output() {
-    if (check == 1) {
-        outputs[0].innerText = parseInt(dateValues[2]) - parseInt(inputs[0].value) 
-        outputs[1].innerText = parseInt(dateValues[0]) - parseInt(inputs[1].value) 
-        outputs[2].innerText = parseInt(dateValues[1]) - parseInt(inputs[2].value) 
-    }
-}
-
-console.log(dateValues)
-
-
-
-
+    // date format DD.MM.YY
+    if (check == 0) {
+        outputs[0].innerText = parseInt(rawDate[0]) - parseInt(inputs[0].value) 
+        outputs[1].innerText = parseInt(rawDate[1]) - parseInt(inputs[1].value) 
+        outputs[2].innerText = parseInt(rawDate[2]) - parseInt(inputs[2].value) 
+    }        
+})
